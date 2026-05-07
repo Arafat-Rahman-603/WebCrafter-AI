7ac6374f716cbb08754d9e17419c88b97e755393
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-05-07 21:29:43 +0600

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,14 @@
 import HomePage from "@/page/HomePage"; 
+import Script from "next/script";
 
 export default function Home() {
   return (
     <>
       <HomePage />
+      <Script
+          src="https://aether-ai-support.vercel.app/AetherAI.js"
+          data-business-id="user_3DOvzYegtE9X4o0YzrpUjAF0ymN" 
+      />
     </>
   );
 }
