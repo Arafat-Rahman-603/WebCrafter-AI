4664d209c31adc8a8121e493e594bc9e5d0337e6
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-05-09 15:54:14 +0600

    Add AetherAI script to HomePage component

--- frontend/src/page/HomePage.jsx
+++ frontend/src/page/HomePage.jsx
@@ -1,11 +1,16 @@
 import Navber from "@/componentes/Navber";
 import Hero from "@/componentes/Hero";
+import Script from "next/script";
 
 export default function HomePage() {
   return (
     <>
     <Navber />
     <Hero />
+       <Script
+          src="https://aether-ai-support.vercel.app/AetherAI.js"
+          data-business-id="user_3DOvzYegtE9X4o0YzrpUjAF0ymN" 
+      />
     </>
   )
 }
