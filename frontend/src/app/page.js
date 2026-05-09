c6e6787956998aa3483f05ed958d83736a743eb4
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-05-09 15:53:38 +0600

    Remove external script from Home component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,9 @@
 import HomePage from "@/page/HomePage"; 
-import Script from "next/script";
 
 export default function Home() {
   return (
     <>
       <HomePage />
-      <Script
-          src="https://aether-ai-support.vercel.app/AetherAI.js"
-          data-business-id="user_3DOvzYegtE9X4o0YzrpUjAF0ymN" 
-      />
     </>
   );
 }
