14af212b6181d3e921dbd077f46a8e738ad8c41c
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-05-25 00:39:11 +0600

    Refactor auth routes to use Google authentication

--- backend/route/auth.route.js
+++ backend/route/auth.route.js
@@ -1,25 +1,15 @@
 import express from "express";
-import { 
-    signup, 
-    verifyEmail, 
-    login, 
-    logout, 
-    forgotPassword, 
-    resetPassword, 
-    checkAuth 
+import {
+    googleAuth,
+    logout,
+    checkAuth
 } from "../controller/auth.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/check-auth", verifyToken, checkAuth);
-
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/google", googleAuth);
 router.post("/logout", logout);
-router.post("/verify-email", verifyEmail);
-
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password/:token", resetPassword);
 
 export default router;
